e1e35911bc76b8c2c18d396fb4cbcb9edfb2c7b0
Mountain Cat <tomasz.banas123@gmail.com>
2023-04-04 03:11:34 +0200

    ⚔

--- frontend/frontend/src/app/services/auth.service.ts
+++ frontend/frontend/src/app/services/auth.service.ts
@@ -3,9 +3,9 @@ import {SocialUser} from "@abacritt/angularx-social-login";
 import {CookieService} from "ngx-cookie-service";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "src/environments/environment";
-import {urlJoin} from "@angular-devkit/build-angular/src/utils";
 import {firstValueFrom} from "rxjs";
 import {AuthRequestModel} from "../models/authRequestModel";
+import {join} from "@angular/compiler-cli";
 
 @Injectable({
   providedIn: 'root'
@@ -25,7 +25,7 @@ export class AuthService {
   public async authUser(authRequest: AuthRequestModel): Promise<string | undefined> {
     try {
       // Fetch user token from backend
-      let authToken = await firstValueFrom(this.http.post<string>(urlJoin(this.apiUri, "auth"), authRequest));
+      let authToken = await firstValueFrom(this.http.post<string>(join(this.apiUri, "auth"), authRequest));
 
       // Set token to cookies
       this._cookieService.set("auth_token", authToken);
